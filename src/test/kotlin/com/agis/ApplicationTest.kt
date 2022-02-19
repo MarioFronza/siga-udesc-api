10e90220fa98ad33bdfffa4ddf37aa52db9a1c4c
MarioFronza <mariofronza1@gmail.com>
2022-02-19 15:09:12 -0300

    first commit

---
+++ src/test/kotlin/com/agis/ApplicationTest.kt
@@ -0,0 +1,13 @@
+package com.agis
+
+import io.ktor.server.routing.*
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.request.*
+import kotlin.test.*
+import io.ktor.server.testing.*
+import com.agis.plugins.*
+
+class ApplicationTest {
+}
